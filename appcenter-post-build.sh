203244982c4b1885e64d99a07393cc4ae0ee76e2
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-02-28 12:55:09 +0300

    Upd

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -12,7 +12,7 @@ github_notify_build_passed() {
   -H "Content-Type: application/json" \
   --data "{
           \"state\": \"success\",
-          \"target_url\": \"https://appcenter.ms/${prbuild_appcenter_owner_type}/${prbuild_appcenter_owner}/apps/${prbuild_appcenter_app}/build/branches/${APPCENTER_BRANCH}/build/${APPENTER_BUILD_ID}\",
+          \"target_url\": \"https://appcenter.ms/${prbuild_appcenter_owner_type}/${prbuild_appcenter_owner}/apps/${prbuild_appcenter_app}/build/branches/${APPCENTER_BRANCH}/builds/${APPENTER_BUILD_ID}\",
           \"description\": \"App Center build successfully created.\",
           \"context\": \"appcenter-ci/${prbuild_appcenter_app}\"
         }" \
@@ -26,7 +26,7 @@ github_notify_build_failed() {
   -H "Content-Type: application/json" \
   --data "{
           \"state\": \"failure\",
-          \"target_url\": \"https://appcenter.ms/${prbuild_appcenter_owner_type}/${prbuild_appcenter_owner}/apps/${prbuild_appcenter_app}/build/branches/${APPCENTER_BRANCH}/build/${APPENTER_BUILD_ID}\",
+          \"target_url\": \"https://appcenter.ms/${prbuild_appcenter_owner_type}/${prbuild_appcenter_owner}/apps/${prbuild_appcenter_app}/build/branches/${APPCENTER_BRANCH}/builds/${APPENTER_BUILD_ID}\",
           \"description\": \"Errors occurred during App Center build.\",
           \"context\": \"appcenter-ci/${prbuild_appcenter_app}\"
         }" \
